fb160d0a461d92255fd7fb14058c159bc036acd7
firststef <firstbossspike@gmail.com>
2021-01-01 03:04:35 +0200

    Redid argument parsing

--- diffupdate.py
+++ diffupdate.py
@@ -1,4 +1,3 @@
-import argparse
 import sys
 
 
@@ -19,48 +18,57 @@ class DDBFile:
 
 
 def main(argv: list):
-    parser = argparse.ArgumentParser(
-        description='DiffUpdate stores different versions of binary files in an efficient manner'
-                    '- by retaining differences between versions instead of the entire files'
-    )
-    parser.add_help = True
-    parser.add_argument("--create", nargs='+',
-                        help='Creates a version db for the specified file'
-                             'usage:'
-                             'diffupdate create <file.version1> <file.version2> <...>'
-                             'note: the order of the passed files does not matter, you will change between versions either by names or assigned id'
-                        )
-    parser.add_argument("-n", "--name", type=str,
-                        help='By default, the db has the name of the first passed file, use this to change the db name'
-                             'usage:'
-                             'diffupdate create <file.version1> <file.version2> <...> --name my_file_history.ddb'
-                        )
-    parser.add_argument("--update", nargs='+',
-                        help='Using a previously created db, navigate to the specified version'
-                             'usage:'
-                             'diffupdate update <file.version2> <db_file.ddb>'
-                        )
-    # parser.add_argument("list", type=str,
-    #                     help='[FUTURE] Using a previously created db, list all the files in the given db'
-    #                     )
+    help_txt = """
+    DiffUpdate stores different versions of binary files in an efficient manner
+    - by retaining differences between versions instead of the entire files
+    
+    "create":
+        'Creates a version db for the specified file'
+        'usage:'
+        'diffupdate create <file.version1> <file.version2> <...>'
+        'note: the order of the passed files does not matter, you will change between versions either by names or assigned id'
+    
+    "--name"
+        'By default, the db has the name of the first passed file, use this to change the db name'
+        'usage:'
+        'diffupdate create <file.version1> <file.version2> <...> --name my_file_history.ddb'
+    
+    "update"
+        'Using a previously created db, navigate to the specified version'
+        'usage:'
+        'diffupdate update <file.version2> <db_file.ddb>'
+    
+    "list"
+    '[FUTURE] Using a previously created db, list all the files in the given db'
+    """
 
-    args = parser.parse_args(args=argv)
+    if len(sys.argv) == 1 or sys.argv[1] == 'help':
+        print(help_txt)
+    elif sys.argv[1] == 'create':
+        db_name = None
+        if len(sys.argv) >= 4:
+            for i in range(2, len(sys.argv)-1):
+                if sys.argv[i] == '-n' or sys.argv[i] == '--name':
+                    db_name = sys.argv[i+1]
+        else:
+            print('Too few names passed')
+            exit(0)
 
-    if args.create:
-        db_name = args.name if args.name else args.create[0].split('.')[0]
-        db = DDBFile(db_name, args.create)
+        db_name = db_name if db_name else sys.argv[2].split('.')[0]
+        db = DDBFile(db_name, sys.argv[2:])
 
         db.dump()
         print('Created new db: ' + db.out_file)
-    if args.update:
-        if args.name:
-            db_name = args.name
-            assert len(args.update) == 1, "Expected 1 parameter but a different number was provided"
-        else:
-            assert len(args.update) == 2, "Incorrect parameters for update command"
-            db_name = args.update[1]
+    elif sys.argv[1] == 'update':
+        assert len(sys.argv) >= 4, "Too few names passed"
+        db_name = None
+        if len(sys.argv) == 5:
+            if sys.argv[3] == '-n' or sys.argv[3] == '--name':
+                db_name = sys.argv[4]
+        db_name = db_name if db_name else sys.argv[3].split('.')[0]
 
-        DDBFile(db_name).load().goto(args.update[0])
+        print('Updating')
+        DDBFile(db_name).load().goto(sys.argv[2])
 
 
 if __name__ == "__main__":
